abadca36020ebc59570f04198e6da437755fedf6
Thiago Gomes Parzanini <parzanini@hotmail.com>
2024-03-29 00:31:39 +0000

    First Commit

--- src/gui/LottoGui.java
+++ src/gui/LottoGui.java
@@ -1,8 +1,19 @@
 package gui;
 
+import classes.LottoButton;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class LottoGui {
+
+    int[] numbersPicked = new int[4];
+    int counter = 0;
+    String[] numbersPickedText = {"N. Picked 1", "N. Picked 2", "N. Picked 3", "N. Picked 4"};
+    ArrayList<LottoButton> buttons = new ArrayList<>();
+
     private JPanel rootPanel;
     private JButton a1Button;
     private JButton a2Button;
@@ -31,14 +42,14 @@ public class LottoGui {
     private JButton a25Button;
     private JButton a26Button;
     private JButton a27Button;
-    private JButton a29Button;
+    private JButton a28Button;
     private JButton nPicked1Button;
     private JButton nPicked2Button;
     private JButton nPicked3Button;
     private JButton nPicked4Button;
     private JButton quickPickButton;
     private JButton purchaseButton;
-    private JButton clearFullSelectionButton;
+    private JButton clearSelectionButton;
     private JButton makeDrawButton;
     private JButton drawN1Button;
     private JButton drawN2Button;
@@ -48,11 +59,249 @@ public class LottoGui {
     private JTextField textField1;
     private JTextField textField2;
 
+    public LottoGui() {
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(1);
+                buttonUpdate();
+            }
+        });
+        a2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(2);
+                buttonUpdate();
+            }
+        });
+        a3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(3);
+                buttonUpdate();
+            }
+        });
+        a4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(4);
+                buttonUpdate();
+            }
+        });
+        a5Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(5);
+                buttonUpdate();
+            }
+        });
+        a6Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(6);
+                buttonUpdate();
+            }
+        });
+        a7Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(7);
+                buttonUpdate();
+            }
+        });
+        a8Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(8);
+                buttonUpdate();
+            }
+        });
+        a9Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(9);
+                buttonUpdate();
+            }
+        });
+        a10Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(10);
+                buttonUpdate();
+            }
+        });
+        a11Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(11);
+                buttonUpdate();
+            }
+        });
+        a12Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(12);
+                buttonUpdate();
+            }
+        });
+        a13Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(13);
+                buttonUpdate();
+            }
+        });
+        a14Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(14);
+                buttonUpdate();
+            }
+        });
+        a15Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(15);
+                buttonUpdate();
+            }
+        });
+        a16Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(16);
+                buttonUpdate();
+            }
+        });
+        a17Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(17);
+                buttonUpdate();
+            }
+        });
+        a18Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(18);
+                buttonUpdate();
+            }
+        });
+        a19Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(19);
+                buttonUpdate();
+            }
+        });
+        a20Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(20);
+                buttonUpdate();
+            }
+        });
+        a21Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(21);
+                buttonUpdate();
+            }
+        });
+        a22Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(22);
+                buttonUpdate();
+            }
+        });
+        a23Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(23);
+                buttonUpdate();
+            }
+        });
+        a24Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(24);
+                buttonUpdate();
+            }
+        });
+        a25Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(25);
+                buttonUpdate();
+            }
+        });
+        a26Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(26);
+                buttonUpdate();
+            }
+        });
+        a27Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(27);
+                buttonUpdate();
+            }
+        });
+        a28Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkCounter(28);
+                buttonUpdate();
+            }
+        });
+quickPickButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                quickPick();
+                buttonUpdate();
+            }
+        });
+    }
+
+    private void quickPick() {
+        for (int i = 0; i < 4; i++) {
+            int x = (int) (Math.random() * 28) + 1;
+            checkCounter(x);
+        }
+    }
+
+    private void buttonUpdate() {
+        nPicked1Button.setText(numbersPickedText[0]);
+        nPicked2Button.setText(numbersPickedText[1]);
+        nPicked3Button.setText(numbersPickedText[2]);
+        nPicked4Button.setText(numbersPickedText[3]);
+    }
+
+    private void checkCounter(int x) {
+        for (LottoButton button : buttons) {
+            if (button.getNumber() == x) {
+                JOptionPane.showMessageDialog(null, "You have already picked this number", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+        }
+        if (counter < 4) {
+            numbersPicked[counter] = x;
+            numbersPickedText[counter] = String.valueOf(x);
+            counter++;
+            buttons.add(new LottoButton(x));
+            JOptionPane.showMessageDialog(null, "Number " + x + " has been picked", "Success", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "You have already picked 4 numbers", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Napoleon Cricket Club Lotto");
         frame.setContentPane(new LottoGui().rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack( );
+        frame.pack();
         frame.setVisible(true);
     }
 }
