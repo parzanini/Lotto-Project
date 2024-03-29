4209fbca6cb56dd8661433d082451bf464c5d76f
Thiago Gomes Parzanini <parzanini@hotmail.com>
2024-03-29 00:31:39 +0000

    Buttons from 1 to 28 and Arrays working

---
+++ src/classes/LottoButton.java
@@ -0,0 +1,21 @@
+package classes;
+
+public class LottoButton {
+
+    private int number;
+
+
+    public LottoButton(int number) {
+        this.number = number;
+
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+}
